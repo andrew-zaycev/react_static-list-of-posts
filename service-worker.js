76fc556a323e40c6ff46343c06d3dda60f12c60a
RioDron <riopositivniy@gmail.com>
2021-01-13 13:43:54 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.1b6ea50562cf8abf248f73f3d229fa90.js"
+  "./precache-manifest.9c9aab9ca04cac96197c18d218d76e33.js"
 );
 
 self.addEventListener('message', (event) => {
